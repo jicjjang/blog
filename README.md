1bcc8227bcf480ced989cc8478aa49ce48aef57a
jicjjang <jicjjang12@gmail.com>
2018-01-23 22:21:05 +0900

    append nuxtjs lib

---
+++ README.md
@@ -0,0 +1,22 @@
+# jicjjang.github.io
+
+> This blog has been used since 2018 using Nuxtjs with vuejs
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install # Or yarn install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, checkout the [Nuxt.js docs](https://github.com/nuxt/nuxt.js).
---
+++ nuxt.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'jicjjang.github.io',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'This blog has been used since 2018 using Nuxtjs with Vuejs' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  router: {
+    base: '/'
+  }
+}

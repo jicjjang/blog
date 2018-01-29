3c57d1e6fac01fd54a146797d9322353256af3f4
jicjjang <jicjjang12@gmail.com>
2018-01-30 00:57:41 +0900

    append load matedata in store

---
+++ store/index.js
@@ -0,0 +1,30 @@
+import Vuex from 'vuex'
+import ContentsMap from '../posts/contentsMap.js'
+
+const store = () => new Vuex.Store({
+  state: {
+    baseUrl: 'http://localhost:3000/blog',
+    // baseUrl: 'https://jicjjang.github.io'
+    postList: ContentsMap.post,
+    categoryList: ContentsMap.category
+  },
+  getters: {
+    baseUrl: state => {
+      return state.baseUrl
+    },
+    postList: state => {
+      return state.postList
+    },
+    categoryList: state => {
+      return state.categoryList
+    }
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  }
+});
+
+export default store

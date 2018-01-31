f7fe2188427f6f70f418fb3d282ba9c29776fe82
jicjjang <jicjjang12@gmail.com>
2018-01-31 18:16:08 +0900

    modified base url

--- store/index.js
+++ store/index.js
@@ -4,7 +4,7 @@ import ContentsMap from '../posts/contentsMap.js'
 const store = () => new Vuex.Store({
   state: {
     // baseUrl: 'http://localhost:3000/blog',
-    baseUrl: 'https://jicjjang.github.io/yet_blog',
+    baseUrl: 'https://jicjjang.github.io/blog',
     postList: ContentsMap.post,
     categoryList: ContentsMap.category
   },

3da761170b6705ac824dd81fa9b025e5a5465f2b
jicjjang <jicjjang12@gmail.com>
2018-02-04 00:35:49 +0900

    append index files for all nested routes

---
+++ pages/category/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <app-home categoryName="javascript" />
+</template>
+
+<script>
+  import Home from '~/pages'
+
+  export default {
+    components: {
+      'appHome': Home
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ pages/page/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <app-home :pageIndex="1" />
+</template>
+
+<script>
+  import Home from '~/pages'
+
+  export default {
+    components: {
+      'appHome': Home
+    }
+  }
+</script>
